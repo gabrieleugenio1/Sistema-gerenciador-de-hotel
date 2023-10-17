916eb949893fc0a96728d42b1f3f6b2a594e8cff
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-10-17 20:01:55 -0300

    refactor: change urlencoded

--- src/configs/configs.mjs
+++ src/configs/configs.mjs
@@ -11,7 +11,7 @@ function configExpress(express, app) {
   //Configurando express
   app.set("view engine", "ejs");
   app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
 
   //Configurando caminhos
   app.set("views", path.join("src",'views'));
